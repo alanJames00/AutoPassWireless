6d406c5654e919bc6fb2ecfabf05a12a5382a980
alanjames00 <alanjames6282@gmail.com>
2023-10-17 23:48:39 +0530

    fix dom elements

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,22 @@
-console.log('Hello from autoums');
+console.log('✈️ AutoUMS in action...');
+
+let loginField = document.querySelector(".login-field")
+console.log(loginField.children);
+
+let userNameField = loginField.children[1].children[1];
+let passwordField = loginField.children[2].children[1];
+let loginBtn = loginField.children[4].firstChild;
+
+let termsAgree = document.querySelector(".login-section");
+let termsAgreeBtn = termsAgree.children[2].firstChild
+console.log(termsAgreeBtn);
+userNameField.setAttribute("value", "12213701");
+passwordField.setAttribute("value", "J4&2ko@0");
+
+
+function loginFn(){
+    termsAgreeBtn.click();
+    loginBtn.click();
+}
+
+setTimeout(loginFn ,1000);  

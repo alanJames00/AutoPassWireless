3ec58dd33bc5587e6260e82197f3baccd9dedb3f
alanjames00 <alanjames6282@gmail.com>
2023-10-18 00:01:30 +0530

    cleanup content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,15 @@
 console.log('✈️ AutoUMS in action...');
 
 let loginField = document.querySelector(".login-field")
-console.log(loginField.children);
+let termsAgree = document.querySelector(".login-section");
+
 
 let userNameField = loginField.children[1].children[1];
 let passwordField = loginField.children[2].children[1];
 let loginBtn = loginField.children[4].firstChild;
-
-let termsAgree = document.querySelector(".login-section");
 let termsAgreeBtn = termsAgree.children[2].firstChild
-console.log(termsAgreeBtn);
+
+
 userNameField.setAttribute("value", "12213701");
 passwordField.setAttribute("value", "J4&2ko@0");
 
